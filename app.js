a80201b80f5b4e1ad0e79623e06ed3b4052dcccc
Oriabure Francis Abulu <80348515+abuluoria@users.noreply.github.com>
2021-06-14 19:38:48 -0400

    change message

--- app.js
+++ app.js
@@ -6,7 +6,3 @@ app.get('/', (req, res) => res.send('WELCOME TO MY WEBPAGE FOR ORIABURE ABULU'))
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
-
-git add app.js
-git commit -m "change message"
-git push
